895b43ca49325bb7f18bb09ff718255520b13b5b
mun <git@t3hmun.com>
2019-03-15 08:27:43 +0000

    Made a facade.

---
+++ size.go
@@ -0,0 +1,26 @@
+// Package t3hterm is currently just a facade for "github.com/olekukonko/ts", which works on Linux, Windows and more.
+package t3hterm
+
+import "github.com/olekukonko/ts"
+
+// SizeAndPosition is exactly what it says it is.
+type SizeAndPosition struct {
+	Rows int
+	Cols int
+	PosX int
+	PosY int
+}
+
+// GetSizeAndPosition talks to the system to get the current size and postion of the terminal.
+func GetSizeAndPosition() (ws SizeAndPosition, err error) {
+	return getSizeAndPosition()
+}
+
+func getSizeAndPosition() (ws SizeAndPosition, err error) {
+	size, err := ts.GetSize()
+	sp := SizeAndPosition{size.Col(),
+		size.Row(),
+		size.PosX(),
+		size.PosY()}
+	return sp, err
+}
